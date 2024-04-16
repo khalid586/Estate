7a17ed590e95b4fa268f791f02c52bc6d97d2fb5
khalid586 <khalidcomilla58@gmail.com>
2024-04-16 07:12:50 +0600

    profile page created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,31 +3,37 @@ import { Link, NavLink } from 'react-router-dom';
 import { Button, Navbar } from 'flowbite-react';
 import { AuthContext } from '../providers/AuthProvider';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function NavbarComp() {
 
   const style = 'rounded-3xl px-4 py-2 border-2 border-green-400';
   const {user,logOut} = useContext(AuthContext);
 
   return (
-    <Navbar className='font-bold'>
-      <NavLink to = "/" className='flex'>
-        <img width={20} src="images/utensils-solid.svg" alt="" />
-        <span className="pl-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white">Books of Khalid</span>
-      </NavLink>
-      <div className="flex md:order-2">
-        {
-          user ? <p className='flex'><button className='flex gap-2 items-center text-white px-3 py-2 mr-2 bg-red-600 rounded-3xl' onClick={logOut}><img className='w-[30px] h-[30px] rounded-full' src={user.photoURL} alt='' />Logout</button></p>:
-          <NavLink to = '/login'  className={({isActive})=>isActive?`px-4 py-2 rounded-3xl bg-green-400 text-white `:`${style} `}>login</NavLink>
-        }
+    <>    
+        <Navbar className='font-bold'>
+            <NavLink to = "/" className='flex'>
 
-      </div>
-      <Navbar className='flex justify-center md:w-2/3'>
-        <NavLink to="/" className={({isActive})=>isActive?'text-blue-600 duration-500 rounded-full px-4 p-2 bg-blue-50 ':'text-black p-2'}><p className='flex gap-1'><img src='/All assets/building-regular.svg' width={15}/>Home</p></NavLink>
-        {
-          user && <NavLink to="/profile" className={({isActive})=>isActive?'text-blue-600 duration-500 rounded-full px-4 p-2 bg-blue-50':'text-black p-2'}>     <p className='flex gap-1'><img src='/All assets/user-regular.svg' width={15}/>Profile</p></NavLink>
-        }
-      </Navbar> 
-    </Navbar>
+                <span className="pl-2 self-center whitespace-nowrap text-xl font-semibold dark:text-white">Books of Khalid</span>
+            </NavLink>
+            <Navbar className='flex justify-center md:w-2/3'>
+                <NavLink to="/" className={({isActive})=>isActive?'text-blue-600 duration-500 rounded-full px-4 p-2 bg-blue-50 ':'text-black p-2'}><p className='flex gap-1'><img src='/All assets/building-regular.svg' width={15}/>Home</p></NavLink>
+                {
+                    user && <NavLink to="/profile" className={({isActive})=>isActive?'text-blue-600 duration-500 rounded-full px-4 p-2 bg-blue-50':'text-black p-2'}>     <p className='flex gap-1'><img src='/All assets/user-regular.svg' width={15}/>Profile</p></NavLink>
+                }
+            </Navbar> 
+            <div className="flex md:order-2">
+                {
+                    user ? <Link to = '/profile' className='flex'><img className='w-[30px] h-[30px] rounded-full' src={user.photoURL} alt='' /></Link>:
+                    <NavLink to = '/login'  className={({isActive})=>isActive?`px-4 py-2 rounded-3xl bg-green-400 text-white `:`${style} `}>login</NavLink>
+                }
+
+            </div>
+
+        </Navbar>
+        <FontAwesomeIcon icon="fa-solid fa-check-double" />
+    </>
   )
 }
 
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,11 +4,15 @@ import {
 import Home from "../pages/Home";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import ProfilePage from "../pages/ProfilePage";
+import ErrorPage from "../pages/ErrorPage";
   
  export const router = createBrowserRouter([
     {
       path: "/",
       element: <Home></Home>,
+      errorElement:<ErrorPage></ErrorPage>,
+
       children:[
         {
             path:'/login',
@@ -17,6 +21,10 @@ import RegisterPage from "../pages/RegisterPage";
         {
             path:'/register',
             element:<RegisterPage></RegisterPage>
+        },
+        {
+            path:'/profile',
+            element:<ProfilePage></ProfilePage>
         },
       ]
     },
