0e2d94f4098eaef88b8eee7c09f80a75808025d1
khalid586 <khalidcomilla58@gmail.com>
2024-04-17 23:25:46 +0600

    footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
         <div className='font-custom'>
             <Navbar></Navbar> 
             <Outlet></Outlet>
-            
+            <Footer></Footer>
         </div>
 
 
